49175545af4c1d9b6b8e42acc902209a618140b3
Ann-Kathrin <annkathrin@ahorngmbh.de>
2019-11-12 11:14:15 +0100

    Edit teasers file to fix bug from v-for

--- pages/teasers/index.vue
+++ pages/teasers/index.vue
@@ -1,54 +1,41 @@
 <template>
   <div>
+    <b-col md="8">
+        // Search articles
+        <form class="search-field">
+            <span ></span>
+            <input class="" v-model="query" type="search" placeholder="Search...">
+        </form>
 
-      // Search articles
-      <form class="">
-          <span ></span>
-          <input class="" v-model="query" type="search" placeholder="Search...">
-      </form>
-
-
-      <div class="teaser-content" v-for="teasers in filteredList" v-bind:key="teasers">
-          <b-col md="6">
-              <div class="teaser-img">
-                  <img :src="'http://localhost:1337/' + teasers.image.url" alt="">
-                  <canvas width="200" height="200"></canvas>
-              </div>
-                <h3 class="">{{ teasers.headline }}</h3>
-                <p>{{ teasers.content }}</p>
-                // Link to the restaurant using router-link
-                <router-link :to="{ name: 'teasers', params: { id: teasers.id }}" tag="a" class="">See dishes
-                </router-link>
-          </b-col>
-      </div>
-
-      <!-- // If no restaurants have been found
+        <!-- <div class="teaser-content" v-for="teaser in filteredList" :key="teaser"> -->
+          <div class="teaser-content" v-for="(teaser, id) in filteredList" :key="id">
+          <div class="teaser-img">
+              <img :src="'http://localhost:1337/' + teaser.image.url" alt="">
+              <!-- <canvas width="200" height="200"></canvas> -->
+          </div>
+            <h3 class="">{{ teaser.headline }}</h3>
+            <p>{{ teaser.content }}</p>
+            // Link to the teasers using router-link
+            <router-link :to="{ name: 'teasers-id', params: { id: teaser.id }}" tag="a" class="">See acrticles </router-link>
+        </div>
+      </b-col>
+      <!-- // If no teasers have been found
       <div class="" v-if="filteredList.length == 0">
        <img src="https://assets-ouch.icons8.com/preview/19/52de2377-696e-4194-8c63-0a81aef60b4f.png" height="200" width="200">
-       <p>No restaurants found</p>
+       <p>No teasers found</p>
      </div> -->
 
   </div>
 </template>
 
-<style scoped>
-  .teaser-content {
-
-  }
-
-  .teaser-img {
-
-  }
-</style>
-
 <script>
-// Import the restaurants query
+// Import the teasers query
 import teasersQuery from '~/apollo/queries/teaser/teasers'
 
 export default {
   data() {
     return {
-      // Initialize an empty restaurants variabkle
+      // Initialize an empty teasers variabkle
       teasers: [],
       query: ''
     }
