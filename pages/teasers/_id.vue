f9601d12a13f5b80a2d0c3faaf39d19a1009c918
Ann-Kathrin <annkathrin@ahorngmbh.de>
2019-11-12 16:45:32 +0100

    add new teaser page for the groups

--- pages/teasers/_id.vue
+++ pages/teasers/_id.vue
@@ -1,43 +1,34 @@
-<template>  
+<template>
 <div>
 
-  
-  <a class="uk-button uk-button-primary uk-margin" @click="$router.go(-1)"><span uk-icon="arrow-left"></span> go back</a>
+
+  <a class="uk-button uk-button uk-margin" @click="$router.go(-1)"><span uk-icon="arrow-left"></span> go back</a>
 
   <client-only>
   <div uk-grid>
-
-      
-      <div class="uk-width-1-3@m">
+      <b-col md="6">
         <div v-for="(group, id) in teaser.groups" :key="id" class="uk-margin">
             <div class="uk-card uk-card-default">
-                <div class="uk-card-media-top">
+                <div class="group-img">
                     <img :src="'http://localhost:1337/' + group.image.url" alt="" />
                 </div>
                 <div class="uk-card-body">
                     <h3 class="uk-card-title">{{ group.name }} </h3>
                     <p>{{ group.description }}</p>
                 </div>
-                <div class="uk-card-footer">
-                  // Doing nothing for the moment :)
-                  <button class="uk-button uk-button-primary">Add to cart</button>
-                </div>
             </div>
         </div>
-      </div>
-      <div class="uk-width-expand@m">
-      </div>
-
+      </b-col>
   </div>
 
   </client-only>
-</div>  
+</div>
 </template>
 
-<script>  
+<script>
 import teaserQuery from '~/apollo/queries/teaser/teaser'
 
-export default {  
+export default {
   data() {
     return {
       teaser: Object
