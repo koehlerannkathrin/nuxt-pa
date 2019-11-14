0178e989fe51a7410853840faa2010bfd5318fe0
Ann-Kathrin <annkathrin@ahorngmbh.de>
2019-11-14 15:04:15 +0100

    Add container

--- components/Header.vue
+++ components/Header.vue
@@ -5,18 +5,18 @@
     <b-nav-item to="/teasers" exact exact-active-class="active">Test</b-nav-item>
     <b-nav-item to="/" exact exact-active-class="active">Contact</b-nav-item>
   </b-nav> -->
-
+  <b-container fluid>
   <b-navbar toggleable="sm" type="light" variant="light">
-    <b-container>
-      <b-col md="2" class="home-link">
+  <b-container>
+      <b-col md="4" class="home-link">
         <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
         <nuxt-link to="/">
           <b-navbar-brand class="home-logo">HOME</b-navbar-brand>
         </nuxt-link>
       </b-col>
-      <b-col md="10">
+      <b-col >
         <b-collapse id="nav-text-collapse" is-nav>
-          <b-navbar-nav >
+          <b-navbar-nav>
             <b-nav-item to="/" exact exact-active-class="active">About us</b-nav-item>
             <b-nav-item to="/infos" exact exact-active-class="active">News</b-nav-item>
             <b-nav-item to="/teasers" exact exact-active-class="active">Test</b-nav-item>
@@ -24,8 +24,7 @@
           </b-navbar-nav>
         </b-collapse>
       </b-col>
-    </b-container>
-
+  </b-container>
   </b-navbar>
-
+  </b-container>
 </template>
--- pages/teasers/_id.vue
+++ pages/teasers/_id.vue
@@ -1,27 +1,27 @@
 <template>
 <div>
-
+<b-container class="groups">
 
   <a class="uk-button uk-button uk-margin" @click="$router.go(-1)"><span uk-icon="arrow-left"></span> go back</a>
 
   <client-only>
-  <div uk-grid>
-      <b-col md="6">
-        <div v-for="(group, id) in teaser.groups" :key="id" class="uk-margin">
-            <div class="uk-card uk-card-default">
-                <div class="group-img">
-                    <img :src="'http://localhost:1337/' + group.image.url" alt="" />
-                </div>
-                <div class="uk-card-body">
-                    <h3 class="uk-card-title">{{ group.name }} </h3>
-                    <p>{{ group.description }}</p>
-                </div>
-            </div>
-        </div>
-      </b-col>
-  </div>
-
+    <div uk-grid>
+        <b-col md="6">
+          <div v-for="(group, id) in teaser.groups" :key="id" class="uk-margin">
+              <div class="uk-card uk-card-default">
+                  <div class="group-img">
+                      <img :src="'https://strapi-pa-heroku.herokuapp.com/' + group.image.url" alt="" />
+                  </div>
+                  <div class="uk-card-body">
+                      <h3 class="uk-card-title">{{ group.name }} </h3>
+                      <p>{{ group.description }}</p>
+                  </div>
+              </div>
+          </div>
+        </b-col>
+    </div>
   </client-only>
+</b-container>
 </div>
 </template>
 
