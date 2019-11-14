16fb5aeb893a38f11c0a46bff4fa9968786bfe5f
Ann-Kathrin <annkathrin@ahorngmbh.de>
2019-11-14 15:03:12 +0100

    Add Footer to the Layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,20 +2,19 @@
   <div>
     <Header />
 
-      <div class="uk-section uk-section-default">
-      <div class="uk-container uk-container-medium">
-
        <nuxt />
-      </div>
-    </div>
+
+     <Footer />
   </div>
 </template>
 
 <script>
 import Header from '~/components/Header.vue'
+import Footer from '~/components/Footer.vue'
 export default {
   components: {
-    Header
+    Header,
+    Footer
   }
 }
 </script>
